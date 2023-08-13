ff41126d04cf11f20707041795913441989f0137
Jdure <22172148+Jdure@users.noreply.github.com>
2023-08-13 17:59:57 -0400

    WIP about template 3

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,25 @@ module.exports = {
             screens: {
                 '3xl': '2560px',
             },
+            animation: {
+                bubble: 'bubble 8s infinite',
+            },
+            keyframes: {
+                bubble: {
+                    '0%': {
+                        transform: 'scale(1)',
+                    },
+                    '33%': {
+                        transform: 'scale(1.2)',
+                    },
+                    '66%': {
+                        transform: 'scale(0.8)',
+                    },
+                    '100%': {
+                        transform: 'scale(1)',
+                    },
+                },
+            },
         },
     },
     plugins: [],
