480d0e95b9437af48f6282d1871fa9bcd929852c
Jdure <22172148+Jdure@users.noreply.github.com>
2024-10-07 22:24:51 -0400

    added pocketbase

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,8 @@ import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
 
-import node from '@astrojs/node'
-import { loadEnv } from 'vite'
-
-const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(
-    process.env.NODE_ENV,
-    process.cwd(),
-    ''
-)
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED
-
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
     output: 'server',
-    adapter: node({
-        mode: 'standalone',
-        ssl: {
-            enabled: true,
-        },
-    }),
 })
